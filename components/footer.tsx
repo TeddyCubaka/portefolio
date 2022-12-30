13f3ff63f2e9479e3a904b04a6743883cc1e13f2
teddy cubaka <teddycubaka96@gmail.com>
2022-12-30 16:18:32 +0100

    Add the rel=noreferrer attribute in every tags which doesn't have it

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,17 @@ export default function Footer() {
 	return (
 		<footer>
 			<div className="main_icons">
-				<a href="https://github.com/TeddyCubaka" target="_blank">
+				<a
+					href="https://github.com/TeddyCubaka"
+					target="_blank"
+					rel="noreferrer">
 					{" "}
 					<BsGithub size="30" />{" "}
 				</a>
 				<a
 					href="https://www.linkedin.com/in/teddy-birhingingwa-a3707a240/"
-					target="_blank">
+					target="_blank"
+					rel="noreferrer">
 					{" "}
 					<BsLinkedin size="30" />{" "}
 				</a>
--- components/main.tsx
+++ components/main.tsx
@@ -15,11 +15,17 @@ export default function Main() {
 			<h1>Teddy Cubaka BIRHINGINGWA</h1>
 			<div>Développeur fullstack</div>
 			<div className="main_icons">
-				<a href="https://github.com/TeddyCubaka" target="_blank">
+				<a
+					href="https://github.com/TeddyCubaka"
+					target="_blank"
+					rel="noreferrer">
 					{" "}
 					<BsGithub size="30" />{" "}
 				</a>
-				<a href="https://www.linkedin.com/in/teddy-birhingingwa-a3707a240/" target="_blank">
+				<a
+					href="https://www.linkedin.com/in/teddy-birhingingwa-a3707a240/"
+					target="_blank"
+					rel="noreferrer">
 					{" "}
 					<BsLinkedin size="30" />{" "}
 				</a>
