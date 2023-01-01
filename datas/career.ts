57b40ebddbd0a558feb4cec9c0e03e0a2ef76001
teddy cubaka <teddycubaka96@gmail.com>
2023-01-01 09:06:01 +0100

    Update description in the career and about component

--- datas/career.ts
+++ datas/career.ts
@@ -16,24 +16,24 @@ const careerArray: LocalDataType[] = [
 		name: "Scolaire",
 		icon: TbSchoolOff,
 		content:
-			"Teddy a fait ses études humanitaire au collège saint Paul Bukavu, au Sud-Kivu. Il finit en mathématique-physique avec un diplôme de 60%. Après avoir obtenu son diplôme d'État, Teddy est venu à Kinshasa pour poursuivre ses études supérieurs. Il a commencé en BTP, avant de tomber de découvrir et de tomber amoureux de la programmation. Une année après son arrivé à kinshasa il a changé de faculté et s'est lancé dans le dévéloppement web.",
-		mobileName: "Scol",
+			"Teddy a fait ses études humanitaires au collège saint Paul Bukavu, au Sud-Kivu. Il a finit en option mathématique et physique avec un diplôme de 60%. Après avoir obtenu son diplôme d'État, Teddy est venu à Kinshasa pour poursuivre ses études supérieurs. Il a commencé en BTP, avant de découvrir et de tomber amoureux de la programmation. Une année après son arrivé à kinshasa il a changé de faculté et s'est lancé dans le dévéloppement web.",
+		mobileName: "Scolaire",
 	},
 	{
 		id: 1,
 		name: "Extra-Scolaire",
 		icon: TbSchool,
 		content:
-			"En déhors de la scolarité, Teddy a fait pas et fait plusieurs activité. Depuis ses 11 ans, il est scout et a bien-sûr très progressé dans le mouvements. Il a fait plusieurs auto-formation, notament en Community Management, qui est le domaine extra-scolaire qu'il exerce toujours.",
-		mobileName: "Extra-Scol",
+			"En déhors de la scolarité, Teddy fait et a fait plusieurs activités. Depuis ses 11 ans, il est scout et a, bien-sûr, beaucoup très progressé dans le mouvement. Il s'est auto-formé dans plusieurs domaines, notament en Community Management, qui est le domaine extra-scolaire qu'il exerce toujours.",
+		mobileName: "Extra-Scol.",
 	},
 	{
 		id: 2,
 		name: "Professionnelle",
 		icon: MdWorkOutline,
 		content:
-			"Dans le domaine de dévéloppement web, Teddy a, jusque là, travaillé chez Kinshasa Digital Academy en tant que développeur fullstack. Dans le domaine de comminuty mangement, il l'a généralement fait dans la maison Jacqueline, une maison des production des jus naturels à Kinshasa.",
-		mobileName: "Prof",
+			"Dans le domaine de dévéloppement web, Teddy a, jusque là, travaillé chez Kinshasa Digital Academy en tant que développeur fullstack. Dans le domaine de comminuty mangement, il l'a généralement fait à la maison Jacqueline, une maison des production des jus naturels à Kinshasa.",
+		mobileName: "Profession.",
 	},
 ];
 
