1d38483b1ed6acb953530f9ce7a526055f91578c
teddy cubaka <teddycubaka96@gmail.com>
2023-09-10 19:21:41 +0100

    style : navbar review

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,140 +5,153 @@ import { FiCopy } from "react-icons/fi";
 import { TfiClose } from "react-icons/tfi";
 
 interface CopyType {
-	mail: boolean;
-	phone: boolean;
+  mail: boolean;
+  phone: boolean;
 }
 
 const Media = () => {
-	const [buttonClass, setButtonClass] = useState("navigations_mobile");
-	const [copy, setCopy] = useState<CopyType>({
-		mail: false,
-		phone: false,
-	});
+  const [buttonClass, setButtonClass] = useState("navigations_mobile");
+  const [copy, setCopy] = useState<CopyType>({
+    mail: false,
+    phone: false,
+  });
 
-	return (
-		<div className={buttonClass} id="navBar">
-			<div
-				onClick={() => {
-					if (buttonClass === "navigations_mobile") {
-						setButtonClass(buttonClass + " " + "navigations_mobile_focus");
-					}
-				}}>
-				<div>
-					<BiMenu size="40" />
-				</div>
-				<p onClick={() => setButtonClass("navigations_mobile")}></p>
-				<div className="sidebar">
-					<div
-						className="menu_closer"
-						onClick={() => setButtonClass("navigations_mobile")}>
-						{" "}
-						<TfiClose size="25" />{" "}
-					</div>
-					<a
-						href="#about"
-						className="navigations_contents"
-						onClick={() => setButtonClass("navigations_mobile")}>
-						A propos
-					</a>
-					<a
-						href="#achievements"
-						className="navigations_contents"
-						onClick={() => setButtonClass("navigations_mobile")}>
-						Réalisations
-					</a>
-					<a
-						href="#career"
-						className="navigations_contents"
-						onClick={() => setButtonClass("navigations_mobile")}>
-						parcours
-					</a>
-					<a
-						href="#skills"
-						className="navigations_contents"
-						onClick={() => setButtonClass("navigations_mobile")}>
-						Compétences
-					</a>
-					<div className="navigations_contents">
-						+243 995 867 384{" "}
-						<button
-							onClick={() => {
-								navigator.clipboard.writeText("teddycubaka96@gmail.com");
-								setCopy({ ...copy, phone: true });
-								setTimeout(() => setCopy({ ...copy, phone: false }), 2000);
-							}}>
-							{copy.phone ? <BsCheckCircleFill /> : <FiCopy />}
-						</button>
-					</div>
-					<div className="navigations_contents">
-						teddycubaka96@gmail.com{" "}
-						<button
-							onClick={() => {
-								navigator.clipboard.writeText("teddycubaka96@gmail.com");
-								setCopy({ ...copy, mail: true });
-								setTimeout(() => setCopy({ ...copy, mail: false }), 2000);
-							}}>
-							{copy.mail ? <BsCheckCircleFill /> : <FiCopy />}
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={buttonClass} id="navBar">
+      <div
+        onClick={() => {
+          if (buttonClass === "navigations_mobile") {
+            setButtonClass(buttonClass + " " + "navigations_mobile_focus");
+          }
+        }}
+      >
+        <div>
+          <BiMenu size="40" />
+        </div>
+        <p onClick={() => setButtonClass("navigations_mobile")}></p>
+        <div className="sidebar">
+          <div
+            className="menu_closer"
+            onClick={() => setButtonClass("navigations_mobile")}
+          >
+            {" "}
+            <TfiClose size="25" />{" "}
+          </div>
+          <a
+            href="#about"
+            className="navigations_contents"
+            onClick={() => setButtonClass("navigations_mobile")}
+          >
+            A propos
+          </a>
+          <a
+            href="#achievements"
+            className="navigations_contents"
+            onClick={() => setButtonClass("navigations_mobile")}
+          >
+            Réalisations
+          </a>
+          <a
+            href="#career"
+            className="navigations_contents"
+            onClick={() => setButtonClass("navigations_mobile")}
+          >
+            parcours
+          </a>
+          <a
+            href="#skills"
+            className="navigations_contents"
+            onClick={() => setButtonClass("navigations_mobile")}
+          >
+            Compétences
+          </a>
+          <div
+            className="navigations_contents"
+            onClick={() => {
+              navigator.clipboard.writeText("+243 995 867 384");
+              setCopy({ ...copy, phone: true });
+              setTimeout(() => setCopy({ ...copy, phone: false }), 2000);
+            }}
+          >
+            +243 995 867 384 {copy.phone ? <BsCheckCircleFill /> : <FiCopy />}
+          </div>
+          <div
+            className="navigations_contents"
+            onClick={() => {
+              navigator.clipboard.writeText("teddycubaka96@gmail.com");
+              setCopy({ ...copy, mail: true });
+              setTimeout(() => setCopy({ ...copy, mail: false }), 2000);
+            }}
+          >
+            teddycubaka96@gmail.com{" "}
+            {copy.mail ? <BsCheckCircleFill /> : <FiCopy />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default function Navbar() {
-	const [showContacts, setShowContacts] = useState(false);
-	const [copy, setCopy] = useState<CopyType>({
-		mail: false,
-		phone: false,
-	});
+  const [showContacts, setShowContacts] = useState(false);
+  const [copy, setCopy] = useState<CopyType>({
+    mail: false,
+    phone: false,
+  });
 
-	return (
-		<div className="navbar">
-			<h1 onClick={() => console.log(window.matchMedia("(max-width : 800px)"))}>
-				Teddy Cubaka
-			</h1>
-			<nav className="navigations">
-				<a href="#about">A propos</a>
-				<a href="#achievements">Réalisations</a>
-				<a href="#career">parcours</a>
-				<a href="#skills">Compétences</a>
-				<button
-					onClick={() => {
-						if (!showContacts) setShowContacts(true);
-						else setShowContacts(false);
-					}}>
-					<BsTelephone size="34px" />
-				</button>
-				{showContacts ? (
-					<div className="contacts_shown">
-						<div className="">
-							+243 995 867 384{" "}
-							<button
-								onClick={() => {
-									navigator.clipboard.writeText("+243 995 867 384");
-									setCopy({ ...copy, phone: true });
-									setTimeout(() => setCopy({ ...copy, phone: false }), 2000);
-								}}>
-								{copy.phone ? <BsCheckCircleFill /> : <FiCopy />}
-							</button>
-						</div>
-						<div className="">
-							teddycubaka96@gmail.com{" "}
-							<button
-								onClick={() => {
-									navigator.clipboard.writeText("teddycubaka96@gmail.com");
-									setCopy({ ...copy, mail: true });
-									setTimeout(() => setCopy({ ...copy, mail: false }), 2000);
-								}}>
-								{copy.mail ? <BsCheckCircleFill /> : <FiCopy />}
-							</button>
-						</div>
-					</div>
-				) : null}
-			</nav>
-			<Media />
-		</div>
-	);
+  return (
+    <div className="navbar">
+      <h1 onClick={() => console.log(window.matchMedia("(max-width : 800px)"))}>
+        Teddy Cubaka
+      </h1>
+      <nav className="navigations">
+        <a href="#about">A propos</a>
+        <a href="#achievements">Réalisations</a>
+        <a href="#career">parcours</a>
+        <a href="#skills">Compétences</a>
+        <button
+          onClick={() => {
+            if (!showContacts) setShowContacts(true);
+            else setShowContacts(false);
+          }}
+          className="ring"
+        >
+          <BsTelephone size="34px" />
+        </button>
+        {showContacts ? (
+          <>
+            <div
+              onClick={() => setShowContacts(false)}
+              className="sub_contacts_shown"
+            ></div>
+            <div className="contacts_shown">
+              <div
+                className=""
+                onClick={() => {
+                  navigator.clipboard.writeText("+243 995 867 384");
+                  setCopy({ ...copy, phone: true });
+                  setTimeout(() => setCopy({ ...copy, phone: false }), 2000);
+                }}
+              >
+                {copy.phone ? <BsCheckCircleFill /> : <FiCopy />}
+                +243 995 867 384{" "}
+              </div>
+              <div
+                className=""
+                onClick={() => {
+                  navigator.clipboard.writeText("teddycubaka96@gmail.com");
+                  setCopy({ ...copy, mail: true });
+                  setTimeout(() => setCopy({ ...copy, mail: false }), 2000);
+                }}
+              >
+                {copy.mail ? <BsCheckCircleFill /> : <FiCopy />}
+                teddycubaka96@gmail.com{" "}
+              </div>
+            </div>
+          </>
+        ) : null}
+      </nav>
+      <Media />
+    </div>
+  );
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import "../styles/globals.mobile.css";
+import "../styles/animation.css"
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
