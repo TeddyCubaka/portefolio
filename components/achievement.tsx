ab426ba96528a3ecee5e37db788bb563cffcd41a
teddy cubaka <teddycubaka96@gmail.com>
2023-09-11 16:07:08 +0100

    style : achievement section

--- components/achievement.tsx
+++ components/achievement.tsx
@@ -4,8 +4,8 @@ import AchievementCard from "./achievementCard";
 export default function Achievement() {
 	return (
 		<section id="achievements">
-			<h2>Qu&#39;a déjà fait Teddy ?</h2>
-			<div className="achievement_block">
+			<h2 className="font-bold text-3xl mb-5 text-[#123853]">Qu&#39;a déjà fait Teddy ?</h2>
+			<div className="grid md:grid-cols-3 gap-5">
 				{achievements.map((real) => (
 					<AchievementCard
 						achievement={real}
--- components/achievementCard.tsx
+++ components/achievementCard.tsx
@@ -3,54 +3,54 @@ import { RiCodeSLine } from "react-icons/ri";
 import { IoOpenOutline } from "react-icons/io5";
 
 interface Achievement {
-	achievement: {
-		name: string;
-		description: string;
-		type: string;
-		technologie: string;
-		link: string;
-		sourceCode: string;
-		frame: string;
-		status: string;
-	};
+  achievement: {
+    name: string;
+    description: string;
+    type: string;
+    technologie: string;
+    link: string;
+    sourceCode: string;
+    frame: string;
+    status: string;
+  };
 }
 
 type LocalStatus = {
-	status: string;
+  status: string;
 };
 
 export default function AchievementCard({ achievement }: Achievement) {
-	return (
-		<div className="achievement_card">
-			<div className="space_between w_max">
-				<div className="meddium base_center color_blue">
-					{" "}
-					{achievement.name}{" "}
-				</div>
-				<span className="status small">{achievement.status}</span>
-			</div>
-			<div className="w_max"> {achievement.description} </div>
-			<div className="space_around w_max">
-				{achievement.link.length > 1 ? (
-					<a className="txt_center" href={achievement.link}>
-						{" "}
-						<IoOpenOutline /> Site web
-					</a>
-				) : (
-					<span>
-						<IoOpenOutline />
-						Site indisponible
-					</span>
-				)}
-				<a className="txt_center" href={achievement.sourceCode}>
-					{" "}
-					<RiCodeSLine /> Code source
-				</a>
-			</div>
-			<div className="space_between small w_max">
-				<span> {achievement.technologie} </span>
-				<span className="color_blue meddium"> {achievement.frame} </span>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-auto border border-solid border-[#123853] flex flex-col items-center p-4 rounded-md ">
+      <div className="space_between w_max">
+        <div className="meddium base_center color_blue">
+          {" "}
+          {achievement.name}{" "}
+        </div>
+        <span className="status small">{achievement.status}</span>
+      </div>
+      <div className="w_max"> {achievement.description} </div>
+      <div className="space_around w_max">
+        {achievement.link.length > 1 ? (
+          <a className="txt_center" href={achievement.link}>
+            {" "}
+            <IoOpenOutline /> Site web
+          </a>
+        ) : (
+          <span>
+            <IoOpenOutline />
+            Site indisponible
+          </span>
+        )}
+        <a className="txt_center" href={achievement.sourceCode}>
+          {" "}
+          <RiCodeSLine /> Code source
+        </a>
+      </div>
+      <div className="space_between small w_max">
+        <span> {achievement.technologie} </span>
+        <span className="color_blue meddium"> {achievement.frame} </span>
+      </div>
+    </div>
+  );
 }
