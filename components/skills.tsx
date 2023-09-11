f23c92ab6e9934c42c16db8a5b0f5f6f41aebef9
teddy cubaka <teddycubaka96@gmail.com>
2023-09-11 16:18:21 +0100

    style : the tools sections

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,34 +1,34 @@
 import skills from "../datas/skills";
 
 interface LocalProps {
-	name: string;
-	Icon: any;
-	color: string;
+  name: string;
+  Icon: any;
+  color: string;
 }
 
 function Skill({ name, Icon, color }: LocalProps) {
-	return (
-		<div className="skill_card">
-			<Icon color={color} size="50" />
-			<span style={{ textAlign: "center" }}> {name} </span>
-		</div>
-	);
+  return (
+    <div className="w-[120px] h-[120px] max-md:w-[85px] max-md:h-[85px] max-md:m-2.5 border border-[#123853] rounded-md m-5 text-center flex flex-col justify-evenly items-center skill_card">
+      <Icon color={color} size="50" />
+      <span style={{ textAlign: "center" }}> {name} </span>
+    </div>
+  );
 }
 
 export default function Skills() {
-	return (
-		<section id="skills">
-			<h2>Voici ce que peut faire Teddy</h2>
-			<div className="skills_section">
-				{skills.map((data, index) => (
-					<Skill
-						name={data.name}
-						color={data.color}
-						Icon={data.icon}
-						key={index}
-					/>
-				))}
-			</div>
-		</section>
-	);
+  return (
+    <section id="skills">
+      <h2 className="font-bold text-3xl mb-5 text-[#123853]">Voici ce que peut faire Teddy</h2>
+      <div className="skills_section">
+        {skills.map((data, index) => (
+          <Skill
+            name={data.name}
+            color={data.color}
+            Icon={data.icon}
+            key={index}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
